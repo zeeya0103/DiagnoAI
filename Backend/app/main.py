bb3c301212b08651806a6a097c83a8fc08dfd65c
Zeeya Sinha <zeeya7981@gmail.com>
2026-07-17 00:34:55 +0530

    Fix Vercel CORS

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -43,7 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://diagnocare-finale.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
